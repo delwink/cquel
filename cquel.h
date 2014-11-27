437e9ea84b09f945d8cb70448451766f8956ef93
David McMackins II <contact@mcmackins.org>
2014-11-27 11:49:03 -0600

    Added software version getter prototype

--- cquel.h
+++ cquel.h
@@ -40,6 +40,12 @@ extern "C"
 const char *cq_interface(void);
 
 /**
+ * @brief Gets the software version.
+ * @return UTF-8 string of the cquel software version.
+ */
+const char *cq_version(void);
+
+/**
  * @brief Initializes the cquel library.
  * @param qlen Maximum length of the query strings used by cquel.
  * @param fmaxlen Length of the buffer for each field and value.
