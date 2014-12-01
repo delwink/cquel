9eca5ff1733b86b706c6a7c878a7e0bd65733fdc
David McMackins II <contact@mcmackins.org>
2014-11-30 22:30:53 -0600

    Remove TODO

--- cquel.c
+++ cquel.c
@@ -833,7 +833,6 @@ int cq_select_query(struct dbconn con, struct dlist **out, const char *q)
         return -3;
     }
 
-    /* TODO: Make this a function */
     MYSQL_FIELD *field;
     size_t i;
     for (i = 0; i < num_fields; ++i) {
@@ -858,7 +857,6 @@ int cq_select_query(struct dbconn con, struct dlist **out, const char *q)
         mysql_free_result(result);
         return rc;
     }
-    /* end TODO */
 
     char *primkey = calloc(CQ_QLEN, sizeof(char));
     if (primkey == NULL) {
